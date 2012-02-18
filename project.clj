1c45eaaec3a43f9cbd0ca062e957f575264b5eed
David Sosby <dsosby@gmail.com>
2012-02-18 14:34:08 -0600

    Update project to use Clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject weightcalc "1.0.0-SNAPSHOT"
   :description "A simple utility to calculate my work weights for the day"
   :main weightcalc.core
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
